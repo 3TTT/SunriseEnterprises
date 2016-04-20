7de86976f93e948ab1db04763648abdd9960485f
psatish <psatish@ADMIN-.pkrmsez.com>
2016-04-20 20:58:44 +0530

    added fashion accessiories with buttons

---
+++ solution/js/script.js
@@ -0,0 +1,14 @@
+// function myFun () {
+// 	document.getElementById("demo").innerHTML="cajkchakd";
+// }
+
+
+function pic1()
+        {
+            document.getElementById("img").src = "images\InteriorDesign.jpg";
+        }
+        
+        function pic2()
+        {
+            document.getElementById("img").src ="picture 2 source";
+        }
