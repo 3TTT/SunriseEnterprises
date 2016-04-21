1448bb5f44b4428b4b806248803ca9bf7f738b57
satish patil <satish patil>
2016-04-21 10:27:25 +0530

    added images for fashion accessories and return function to call the images

--- solution/js/script.js
+++ solution/js/script.js
@@ -3,12 +3,44 @@
 // }
 
 
-function pic1()
-        {
-            document.getElementById("img").src = "images\InteriorDesign.jpg";
-        }
+// function pic1()
+//         {
+//             document.getElementById("img").src = "images\InteriorDesign.jpg";
+//         }
         
-        function pic2()
+//         function pic2()
+//         {
+//             document.getElementById("img").src ="picture 2 source";
+//         }
+
+   function pic1()
         {
-            document.getElementById("img").src ="picture 2 source";
+            document.getElementById("img").src = "images/fashionAccessories/GroomingAccessories.jpg";
+            document.getElementById("img1").src = "images/fashionAccessories/GroomingAccessories1.jpg";
+            document.getElementById("img2").src = "images/fashionAccessories/GroomingAccessories2.jpg";
+            document.getElementById("img3").src = "images/fashionAccessories/GroomingAccessories3.jpg";
         }
+	function pic2(){
+        	document.getElementById("img").src = "images/fashionAccessories/Earrings.jpeg";
+            document.getElementById("img1").src = "images/fashionAccessories/Earrings1.jpeg";
+            document.getElementById("img2").src = "images/fashionAccessories/Earrings2.jpeg";
+            document.getElementById("img3").src = "images/fashionAccessories/Earrings3.jpeg";
+        }       
+	function pic3(){
+        	document.getElementById("img").src="images/fashionAccessories/Watches.jpg";
+            document.getElementById("img1").src = "images/fashionAccessories/Watches1.jpg";
+            document.getElementById("img2").src = "images/fashionAccessories/Watches2.jpg";
+            document.getElementById("img3").src = "images/fashionAccessories/Watches3.jpg";
+        }           
+    function pic4(){
+    		document.getElementById("img").src = "images/fashionAccessories/Bag.jpg"
+            document.getElementById("img1").src = "images/fashionAccessories/Bag1.jpg";
+            document.getElementById("img2").src = "images/fashionAccessories/Bag2.jpg";
+            document.getElementById("img3").src = "images/fashionAccessories/Bag3.jpg";
+        }
+    function pic5(){
+    		document.getElementById("img").src = "images/fashionAccessories/Baby.jpg"
+            document.getElementById("img1").src = "images/fashionAccessories/Baby1.jpg";
+            document.getElementById("img2").src = "images/fashionAccessories/Baby2.jpg";
+            document.getElementById("img3").src = "images/fashionAccessories/Baby3.jpg";
+        }                   
